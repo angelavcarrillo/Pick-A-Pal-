eaed9d8c568f99d416e3fce32a15a83c208e18d1
Angela Carrillo <angelacarrv@icloud.com>
2026-03-16 02:16:04 -0400

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Angela on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
